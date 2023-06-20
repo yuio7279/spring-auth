c7c232b135b75525d03c9a4cec0ee5becc0fc2c1
yuio7279 <yuio7279@gmail.com>
2023-06-20 13:19:05 +0900

    회원가입 구현

---
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.sparta.springauth.controller;
+
+import com.sparta.springauth.dto.SignupRequestDto;
+import com.sparta.springauth.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+
+    @GetMapping("/user/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/user/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @PostMapping("/user/signup")
+    public String  signup(@ModelAttribute SignupRequestDto requestDto){
+        userService.signup(requestDto);
+        return "redirect:/api/user/login-page";
+    }
+}
