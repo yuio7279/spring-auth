cd56acce4cc5437789af0846f911d7fd77d3aeed
yuio7279 <yuio7279@gmail.com>
2023-06-20 22:43:02 +0900

    스프링 시큐리티 JWT 로그인

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -37,13 +37,5 @@ public class UserController {
         return "redirect:/api/user/login-page";
     }
 
-    @PostMapping("/user/login")
-    public String login(@ModelAttribute LoginRequestDto requestDto, HttpServletResponse res){
-        try {
-            userService.login(requestDto, res);
-        } catch (Exception e) {
-            return "redirect:/api/user/login-page?error";
-        }
-        return "redirect:/";
-    }
+
 }
